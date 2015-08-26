cc64d60a8dc7f26f99fbd5250666a3a08b896e67
gawakou <gawakou@gmail.com>
2015-08-26 22:36:23 +0900

    201508372200

---
+++ openCVSamplefacedetection/src/main/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,146 @@
+/Users/ogawa/face-detection/openCVSamplefacedetection/src/main/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ /Users/ogawa/face-detection/openCVSamplefacedetection/src/main/jni/DetectionBasedTracker_jni.cpp \
+ /Users/ogawa/face-detection/openCVSamplefacedetection/src/main/jni/DetectionBasedTracker_jni.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/jni.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /Users/ogawa/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ /Users/ogawa/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/assert.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/string.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/malloc.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/alloca.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/strings.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdint.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/memory.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/math.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/limits.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ /Users/ogawa/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ //Users/ogawa/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm \
+ //Users/ogawa/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath \
+ //Users/ogawa/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef \
+ //Users/ogawa/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/complex \
+ //Users/ogawa/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/map \
+ //Users/ogawa/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+ /Users/ogawa/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ //Users/ogawa/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/limits \
+ /Users/ogawa/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /Users/ogawa/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+ /Users/ogawa/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ //Users/ogawa/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/deque \
+ //Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/log.h
+
+/Users/ogawa/face-detection/openCVSamplefacedetection/src/main/jni/DetectionBasedTracker_jni.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/jni.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/Users/ogawa/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/Users/ogawa/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/assert.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/string.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/strings.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/memory.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/math.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/limits.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+/Users/ogawa/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+//Users/ogawa/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm:
+
+//Users/ogawa/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath:
+
+//Users/ogawa/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef:
+
+//Users/ogawa/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/complex:
+
+//Users/ogawa/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/map:
+
+//Users/ogawa/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+/Users/ogawa/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+//Users/ogawa/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/limits:
+
+/Users/ogawa/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/Users/ogawa/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+/Users/ogawa/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+//Users/ogawa/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/deque:
+
+//Users/ogawa/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/log.h:
